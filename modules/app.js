7a747313c2c8aa528d3ea1f27a8f2c925fa36c91
tarunshahi <tarunarya1993@gmail.com>
2015-06-10 13:56:14 +0530

    change project

--- modules/app.js
+++ modules/app.js
@@ -2,7 +2,7 @@
 
 var drugLord = angular.module('drugLord',[]);
 
-drugLord.controller('gameController',['$scope','cityService','playerService','actionService','warehouseService',function($scope,cityServ,player,action,ware){
+drugLord.controller('gameController',['$scope','cityService','playerService','actionService','warehouseService','placeService',function($scope,cityServ,player,action,ware,place){
 	$scope.start = false;
 
 	$scope.startNewGame = function() {
@@ -11,8 +11,10 @@ drugLord.controller('gameController',['$scope','cityService','playerService','ac
 		cityServ.initCities();
 		$scope.setupPlayer();
 		$scope.setupCityNMarket();
-
-
+        $scope.storInventory=place.initInventory;
+        var startBtn=document.getElementById("start");
+        startBtn.innerHTML="Restart";
+        startBtn.className="btn btn-warning col-xs-5";
 		$scope.start = true;
 	};
 
@@ -35,13 +37,30 @@ drugLord.controller('gameController',['$scope','cityService','playerService','ac
 
 	//other stuff
 	$scope.selectedDrug = cityServ.selectedDrug;
+	
+	//action service
 	$scope.buyDrug = action.buyDrug;
 	$scope.sellDrug = action.sellDrug;
 	$scope.dumpDrug = action.dumpDrug;
-
+   
+     //warehouse Service
 	$scope.whdrugs = ware.whdrugs;
 	$scope.selectedDrugWareHouse = ware.selectedDrugWareHouse;
-
+    
+      //place service
+    $scope.selectedSellInvItem=place.selectInvItem;
+    $scope.buyArray=place.buyItems;
+    $scope.buyInventoryItem=place.buyItem;
+    $scope.sellBuyInvItem=place.sellItem;
+    $scope.selectedBuyInvItems=place.selectedBuyInvItems;
+    $scope.vault=place.vault;
+    $scope.moveInVault=place.pushInvault;
+    $scope.pushInPocket=place.pushInPocket;
+    $scope.selectVaultItems=place.selectVaultItems;
+    $scope.bankOperation = place.bankOperation;
+    $scope.payLone=place.payLone;
+	
+    //watch function
 	$scope.$watch(function(){
 		return player.cash;
 	},function(newValue){
--- modules/place.js
+++ modules/place.js
@@ -1,6 +1,6 @@
 'use strict';
 
-drugLord.service('placeService',['playerService',function(player){
+drugLord.service('placeService',['playerService','warehouseService','cityService',function(player,whs,citys){
 	var place=this;
 	place.initInventory=[
 		       {
@@ -88,7 +88,48 @@ drugLord.service('placeService',['playerService',function(player){
 		       }
 	];
 	place.buyItems=[];
+  place.vault=[];
 	place.prevTarget = null;
+  place.bankOperation = function(){
+        var op=document.getElementsByName("operation");
+            if(op[0].checked)
+            {
+               var cash=parseInt(document.getElementById("ammount").value);
+               if((cash <= player.cash) && (cash >= 0))
+               {
+                   player.cash-=cash;
+                   player.bank+=cash;
+               }
+               else
+               {
+                  window.alert("enter valid ammount");
+               }    
+            }
+            else if(op[1].checked)
+            {
+                var cash=parseInt(document.getElementById("ammount").value);
+                 if((cash <= player.bank) && (cash >= 0))
+               {
+                   player.cash+=cash;
+                   player.bank-=cash;
+               }
+               else
+               {
+                  window.alert("enter valid ammount");
+               }    
+            }
+    };
+    place.payLone=function(){
+       if(player.cash >= player.debt)
+       {
+        player.cash-=player.debt;
+        player.debt-=player.debt;
+       }
+       else
+       {
+        window.alert("Your are Short of money");
+       }
+    };
 	place.selectInvItem=function(e,index){
 
        console.log("inside fun");
@@ -121,7 +162,7 @@ drugLord.service('placeService',['playerService',function(player){
       	if( place.initInventory[i].selected == true)
       	{
       		var temp=parseInt(window.prompt("You want to buy "+ place.initInventory[i].name +" at the price of "+ place.initInventory[i].price+"\n Enter the quantity you want to buy"));
-      	   if(temp!= null)
+      	   if((temp!= null) && (temp > 0) )
       	   {
       	     var requied_cost=place.initInventory[i].price*temp;
       	     if(player.cash > requied_cost)
@@ -133,6 +174,10 @@ drugLord.service('placeService',['playerService',function(player){
       	        console.log(place.buyItems);
       	     }
       	   }
+           else
+           {
+            window.alert("Enter valid value of quantity");
+           }
       	}
       }
   };
@@ -145,9 +190,9 @@ place.sellItem=function(){
       	if(place.buyItems[i].selected == true)
       	{
       		var temp=parseInt(window.prompt("You want to sell "+ place.buyItems[i].name +" at the price of "+ place.buyItems[i].price+"you have avilabe quantity "+place.buyItems[i].qty+"\n Enter the quantity you want to sell"));
-      	   if(temp!= null)
+      	   if(temp!= null) 
       	   {
-           if(temp < place.buyItems[i].qty)
+             if(temp < place.buyItems[i].qty  && temp > 0)
       	     {
       	     	  player.cash+=(temp*place.buyItems[i].price);
       	        place.buyItems[i].qty-=temp
@@ -163,6 +208,7 @@ place.sellItem=function(){
                  window.alert("Enter the quantity in specifed range");
              }
       	   }
+           e
       	}
       }
     }
@@ -195,5 +241,133 @@ place.selectedBuyInvItems=function(e,index){
             place.prevTarget = e.currentTarget;
         }
 };
+place.pushInvault=function(){
+  console.log("inside vault");
+  for(var i=0;i< whs.whdrugs.length;i++)
+  {
+    console.log("inside for");
+    console.log(whs.whdrugs[i].selected);
+    if(whs.whdrugs[i].selected==true)
+    {
+      console.log("inside if");
+      var temp=parseInt(window.prompt("You want to put "+whs.whdrugs[i].name+" into vault.\n You have avilable quantity is "+whs.whdrugs[i].qty+"\n Enter the quantity you want to put inside vault"));
+      if((temp <= whs.whdrugs[i].qty) && (temp > 0) && (!isNaN(temp)))
+      {
+         var flag=check(i,place.vault,whs.whdrugs);
+         if(flag == place.vault.length)
+         { 
+            if(temp < whs.whdrugs[i].qty)
+            {
+               place.vault.push({name:whs.whdrugs[i].name,price:whs.whdrugs[i].price,qty:temp,selected:false,});
+               whs.whdrugs[i].qty-=temp;
+            }
+            else if(temp == whs.whdrugs[i].qty)
+            {
+               place.vault.push({name:whs.whdrugs[i].name,price:whs.whdrugs[i].price,qty:temp,selected:false,});
+               whs.whdrugs.splice(i,1);          
+            }
+          }
+          else
+          {
+             
+             if(temp < whs.whdrugs[i].qty)
+            {
+               place.vault[flag].qty+=temp;
+               whs.whdrugs[i].qty-=temp;
+            }
+            else if(temp == whs.whdrugs[i].qty)
+            {
+              place.vault[flag].qty+=temp;
+               whs.whdrugs.splice(i,1);          
+            }
+          }    
+      }
+      else
+      {
+        window.alert(" Enter value in specifed range ");
+      }
+    }
+  }
+};
+ function check(i,obj1,obj2)
+{
+  var flag=0;
+  for( var j=0;j<obj1.length;j++)
+  {
+       console.log("inside check");
+    if( obj1[j].name == obj2[i].name )
+    {
+      break;
+    }
+    flag++;
+  }
+  return flag;
+}
+place.selectVaultItems=function(e,index){
+    for(var i=0; i < place.vault.length; i++) {
+            place.vault[i].selected = false;
+        }
+       place.vault[index].selected = true;
+        if(place.prevTarget == null) {
+            e.currentTarget.className="list-group-item active";
+            place.prevTarget = e.currentTarget;
+        } else if(place.prevTarget == e.currentTarget) {
+            if(e.currentTarget.className == "list-group-item active") {
+                 e.currentTarget.className = "list-group-item";
+               place.vault[index].selected = false;
+            } else {
+                 e.currentTarget.className = "list-group-item active";
+                place.vault[index].selected = true;
+            }
+        }
+        else {
+            place.prevTarget.className="list-group-item";
+            e.currentTarget.className="list-group-item active";
+            place.prevTarget = e.currentTarget;
+        }
+};
+place.pushInPocket=function()
+{
+  for(var i=0;i< place.vault.length;i++)
+  {
+    if(place.vault[i].selected == true)
+    {
+
+      var temp=parseInt(window.prompt("You want to push "+place.vault[i].name+" into market from vault.\n Enter quantity you want to push "));
+      if( (temp <= place.vault[i].qty) && (temp >0) && (! isNaN(temp)))
+      {
+          var flag=check(i,whs.whdrugs,place.vault);
+          if(flag == whs.whdrugs.length)
+          { 
+            if(temp < place.vault[i].qty)
+            {
+               whs.whdrugs.push({name:place.vault[i].name,price:place.vault[i].price,qty:temp,selected:false});
+               place.vault[i].qty-=temp;
+            }
+            else if(temp == place.vault[i].qty)
+            {
+               whs.whdrugs.push({name:place.vault[i].name,price:place.vault[i].price,qty:temp,selected:false});
+               place.vault.splice(i,1);          
+            }
+          }
+          else
+          {
+             
+            if(temp < place.vault[i].qty)
+            {
+               whs.whdrugs[flag].qty+=temp;
+               place.vault[i].qty-=temp;
+            }
+            else if(temp == place.vault[i].qty)
+            {
+              whs.whdrugs[flag].qty+=temp;
+              place.vault.splice(i,1);          
+            }
+          }  
+      }
+    }
+  }
+};
+
 
 }]);
